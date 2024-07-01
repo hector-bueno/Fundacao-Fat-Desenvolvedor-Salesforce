04f3ba975f509607351ec794207e1347d155cb91
hector-bueno <hrbueno@gmail.com>
2024-07-01 17:04:09 -0300

    Inserido comando useLocale para formatar leitura da Variável Float altura

--- ex01_02_02.java
+++ ex01_02_02.java
@@ -1,10 +1,12 @@
 // 2-2 Faça um algoritmo que leia o nome e a altura de uma pessoa e exiba uma mensagem dizendo "Nome, você tem X metros de altura".
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ex01_02_02 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        ler.useLocale(Locale.US);
 
         System.out.println("Por Favor Insira seu Nome Completo:");
         String nome = ler.nextLine();
@@ -13,8 +15,10 @@ public class ex01_02_02 {
         float altura = ler.nextFloat();
 
         // System.out.print(nome + ", você tem " + altura + " metros de altura");
-        System.out.printf(nome + ", você tem " + altura + " metros de altura");
+        // System.out.printf(nome + ", você tem " + altura + " metros de altura");
+        System.out.printf("%s, você tem %.2f metros de altura%n", nome, altura);
         // System.ot.println(nome + ", você tem " + altura + " metros de altura");
 
+        ler.close();
     }
 }
